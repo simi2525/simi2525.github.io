b571bcf8950951e9c50b31159ece8bf7a214bea0
madalina1 <ioanainsuratelu@yahoo.com>
2023-01-09 21:19:47 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Cristian Simionescu's personal website
-
-We are using Figma for the design system: https://www.figma.com/file/KbXKPGCdaayWo8zea9Lo0Z/Untitled?node-id=0%3A1
--- cristian-simionescu-website/src/components/link/styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledLink = styled.a`
-  color: ${({ theme }) => theme.colors.primary.text};
-  text-decoration: none;
-  width: auto;
-  height: auto;
-`;
