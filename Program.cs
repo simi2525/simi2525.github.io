8e9caeaa79991f90c0e477bb220b9dfc2e24d50b
Cristian Simionescu <cristian@nexusmedia.ro>
2023-10-05 13:31:01 +0300

    Misc

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using MudBlazor.Services;
 
 namespace cristian.simionescu.me
 {
@@ -17,7 +16,6 @@ namespace cristian.simionescu.me
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
