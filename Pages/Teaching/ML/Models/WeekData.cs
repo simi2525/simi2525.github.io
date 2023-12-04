4b489d3108564cf115638128dce07266eab035ef
Cristian Simionescu <cristian@nexusmedia.ro>
2023-12-05 01:43:49 +0200

    Misc

--- Pages/Teaching/ML/Models/WeekData.cs
+++ Pages/Teaching/ML/Models/WeekData.cs
@@ -6,5 +6,6 @@ public class WeekData
     public string UploadFormLink { get; set; }
     public string Homework { get; set; }
     public string Deadline { get; set; }
+    public string AdditionalInformation { get; set; } = "";
     public List<StudentGroup> StudentGroups { get; set; }
 }
