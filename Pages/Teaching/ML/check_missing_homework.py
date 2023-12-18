1f0214edcd76cb034a41fda5a968416e0e04e682
Cristian Simionescu <cristian@nexusmedia.ro>
2023-12-18 21:23:25 +0200

    Week 12

--- Pages/Teaching/ML/check_missing_homework.py
+++ Pages/Teaching/ML/check_missing_homework.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 students = open('students.txt', 'r').readlines()
 homework = open('homework.txt', 'r').readlines()
@@ -11,8 +12,11 @@ not_submitted_students = all_students_set - submitted_students_set
 for student_id in not_submitted_students:
     print(student_id)
 
-
-
+print("\n\nRandomly selected 15 students to check:")
+random_check = random.sample(list(submitted_students_set), 15)
+for student_id in random_check:
+    print(student_id)
+    
 
 
 
